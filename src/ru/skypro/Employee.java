932c91610b514aea2cdd79d084bf1aebdb45866e
M4A1Dephoenix <67238335+M4A1Dephoenix@users.noreply.github.com>
2021-12-06 00:07:31 +0300

    ready easy level

---
+++ src/ru/skypro/Employee.java
@@ -0,0 +1,32 @@
+package ru.skypro;
+
+public class Employee {
+    private final String fio;
+    private final String department;
+    private final int salary;
+    private final int id;
+    public Employee (String fio, String department, int salary, int id){
+        this.fio = fio;
+        this.department = department;
+        this.salary = salary;
+        this.id = id;
+    }
+
+    public String getfio(){return fio;}
+    public String getepartment(){return department;}
+    public int getsalary(){return salary;}
+    public int getid(){return id;}
+    public void setdepartment(String department){ department = department;}
+    public void setsalaryalary(int salary){salary = salary;}
+    public void setid(int id) {id = id;}
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "fio='" + fio + '\'' +
+                ", department='" + department + '\'' +
+                ", salary='" + salary + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,53 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    Employee[] employee = new Employee[2];
+    employee[0] = new Employee("Васильев Василий Васильевич", "1", 100000,0);
+    employee[1] = new Employee("Иванов Иван Иванович", "2", 110000, 1);
+    int allSalary = 0;
+    int lowSalary = 999999999;
+    int hightSalary = 0;
+    int mediumSalary = 0;
+        for (int i = 0; i < employee.length ; i++) {
+
+            int salary = employee[i].getsalary();
+            allSalary = allSalary(salary,allSalary);
+            lowSalary = lowtSalary(salary,lowSalary);
+            hightSalary = hightSalary(salary,hightSalary);
+            mediumSalary = mediumSalary(i+1, mediumSalary,salary);
+
+            System.out.println("ФИО " + employee[i].getfio()+"; " + "Отдел " + employee[i].getepartment() +"; " + "Заработная плата " + employee[i].getsalary());
+
+        }
+        System.out.println("Общая заработная плата = "+ allSalary);
+        System.out.println("Минимальная заработная плата у сотрудника: " + lowSalary);
+        System.out.println("Максимальная заработная плата у сотрудника: " + hightSalary);
+        System.out.println("Средняя заработная плата у сотрудников: " + mediumSalary);
+
+
+    }
+    public static int allSalary(int salary,int allSalary){
+
+        allSalary= allSalary + salary;
+        return allSalary;
+    }
+    public static int lowtSalary(int salary, int lowSalary){
+        if (lowSalary > salary){
+            lowSalary = salary;
+        }
+        return lowSalary;
+    }
+    public static int hightSalary(int salary, int hightSalary)
+    {
+        if (hightSalary < salary){
+        hightSalary = salary;
+        }
+        return hightSalary;
+    }
+    public static int mediumSalary(int i, int  mediumSalary, int salary){
+        mediumSalary =  (mediumSalary+salary) /i;
+        return  mediumSalary;
+
+
     }
 }
